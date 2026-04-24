55e0522d18f31b42a634821a23fa6de653d3e322
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2026-04-24 16:22:46 -0400

    moving along here

--- _2026/jepa/p53_60.py
+++ _2026/jepa/p53_60.py
@@ -29,7 +29,7 @@ class P53_60(InteractiveScene):
             imgs.add(ImageMobject(str(img_dir+'/img_pairs-'+str(i).zfill(2)+'.png')))
 
 
-        svgs_to_skip=[0, 1, 2, 3, 4, 5]
+        svgs_to_skip=[0, 1, 2, 3, 4]
         svg_files=list(sorted(svg_dir.glob('*.svg')))
         all_svgs=Group()
         for i, svg_file in enumerate(svg_files): 
@@ -40,7 +40,7 @@ class P53_60(InteractiveScene):
 
         
 
-        embedding_network_1=all_svgs[0]
+        embedding_network_1=all_svgs[1]
         embedding_network_2=embedding_network_1.copy()
 
         embedding_network_1.move_to([-3, -0.2, 0 ])
@@ -70,7 +70,128 @@ class P53_60(InteractiveScene):
         # self.add(border_0, border_1)
 
 
+        # self.add(all_svgs[2])
+        all_svgs[2].move_to([-3.01, 1.7, 0])
+        all_svgs[3].next_to(all_svgs[2], UP, buff=0.2)
+        all_svgs[4].move_to([-4.5, 0.65, 0]) 
+
         self.wait()
+        self.play(FadeIn(all_svgs[2]),
+                  embedding_network_1[75].animate.set_color(YELLOW),
+                  embedding_network_1[83].animate.set_color(YELLOW),
+                  embedding_network_1[87].animate.set_color(YELLOW),
+                )
+
+        self.wait()
+        self.play(Write(all_svgs[3]), run_time=3)
+
+        self.wait()
+        self.play(Write(all_svgs[4]), run_time=3)
+
+        
+
+        image_border_group_0=Group(imgs[0], border_0)
+        image_border_group_1=Group(imgs[1], border_1)
+
+        embedding_network_1a=embedding_network_1.copy()
+        embedding_network_1a[75].set_color(CHILL_BROWN)
+        embedding_network_1a[83].set_color(CHILL_BROWN)
+        # embedding_network_1a[87].set_color(CHILL_BROWN)
+
+        embedding_network_1a.rotate(-90*DEGREES, [0, 0, 1])
+        embedding_network_1a.move_to([-3, 1.5, 0])
+        embedding_network_1a.scale(0.85)
+
+        embedding_network_2b=embedding_network_2.copy()
+        embedding_network_2b.rotate(-90*DEGREES, [0, 0, 1])
+        embedding_network_2b.move_to([-3, -1.5, 0])
+        embedding_network_2b.scale(0.85)
+
+        self.wait()
+        self.remove(all_svgs[4], all_svgs[3], all_svgs[2])
+        self.play(
+                  #FadeOut(all_svgs[4]),
+                  #FadeOut(all_svgs[3]),
+                  #FadeOut(all_svgs[2]),
+                  Transform(embedding_network_1, embedding_network_1a),
+                  Transform(embedding_network_2, embedding_network_2b),
+                  image_border_group_0.animate.move_to([-5, 1.5, 0]),
+                  image_border_group_1.animate.move_to([-5, -1.5, 0]),
+                  run_time=5)
+
+        # self.remove(all_svgs[4], all_svgs[3], all_svgs[2])
+        # embedding_network_1[75].set_color(CHILL_BROWN)
+        # embedding_network_1[83].set_color(CHILL_BROWN)
+        # embedding_network_1[87].set_color(CHILL_BROWN)
+
+        # embedding_network_1.rotate(-90*DEGREES, [0, 0, 1])
+        # embedding_network_1.move_to([-3, 1.5, 0])
+        # embedding_network_1.scale(0.85)
+
+        # embedding_network_2.rotate(-90*DEGREES, [0, 0, 1])
+        # embedding_network_2.move_to([-3, -1.5, 0])
+        # embedding_network_2.scale(0.85)
+
+        # image_border_group_0.move_to([-5, 1.5, 0])
+        # image_border_group_1.move_to([-5, -1.5, 0])
+
+        # image_border_group_0.next_to(embedding_network_1, LEFT, buff=0.2)
+        # image_border_group_1.next_to(embedding_network_2, LEFT, buff=0.2)
+
+        all_svgs[8].move_to([-1.65, 0.2, 0])
+
+        self.wait()
+        self.add(all_svgs[8][0])
+
+        axes = Axes(
+            x_range=(0, 21, 5),
+            y_range=(-1.2, 1.2, 0.5),
+            width=7.0,
+            height=2.0,
+            axis_config={
+                "color": CHILL_BROWN,
+                "stroke_width": 2,
+                "include_ticks": False,
+                "include_tip": True,
+                "tip_config": {
+                    "width": 0.15,
+                    "length": 0.15,
+                },
+            }
+        )
+        axes.move_to([2.4, 1.7, 0])
+        # self.add(axes)
+
+        # Explicit but semi-random y values
+        y_values = [0.9, -0.8, 0.2, 0.5, -0.3, 0.7, 0.6, 0.1, 0.4, -0.5,
+                    0.75, -0.25, 0.55, 0.7, 0.3, -0.45, -0.6, -0.15, 0.8, 0.35]
+        x_values = list(range(1, 21))
+
+        # Build dots and connecting lines up front
+        dots = VGroup()
+        lines = VGroup()
+        for i, (x, y) in enumerate(zip(x_values, y_values)):
+            dot = Dot(axes.c2p(x, y), radius=0.06)
+            dot.set_color(YELLOW)
+            dots.add(dot)
+            if i > 0:
+                line = Line(
+                    axes.c2p(x_values[i-1], y_values[i-1]),
+                    axes.c2p(x, y),
+                    stroke_width=2,
+                )
+                line.set_color(YELLOW)
+                lines.add(line)
+
+        self.wait()
+
+        self.add(axes)
+        self.add(dots, lines)
+
+
+
+
+
 
 
         self.wait(20)
